e1b25b1e538741bb518bde3c86de771b2d39454e
Mohamed Bayome <mohamed.bayome@nouvelage.fr>
2026-08-30 12:41:58 +0300

    Delete nouvelage-demo-app directory

--- nouvelage-demo-app/app/(auth)/_layout.tsx
+++
@@ -1,5 +0,0 @@
-import { Stack } from 'expo-router';
-import { c } from '../../src/theme';
-export default function L() {
-  return <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: c.bg } }} />;
-}
